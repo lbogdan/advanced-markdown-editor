86bdf5051f1f1d8bc5eb21a45a51e7a4b1366a34
Paul Soporan <paul.soporan@gmail.com>
2020-01-01 17:46:05 +0200

    Increased Vue CLI Type Checking Service Memory Limit

--- vue.config.js
+++ vue.config.js
@@ -1,14 +1,33 @@
+/* eslint-disable no-param-reassign */
+
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
 const MonacoEditorPlugin = require('monaco-editor-webpack-plugin');
 
 module.exports = {
-  configureWebpack: {
-    plugins: [
+  configureWebpack: (config) => {
+    // Get a reference to the existing ForkTsCheckerWebpackPlugin
+    const existingForkTsChecker = config.plugins.filter(
+      (p) => p instanceof ForkTsCheckerWebpackPlugin,
+    )[0];
+
+    // Remove the existing ForkTsCheckerWebpackPlugin
+    config.plugins = config.plugins.filter(
+      (p) => !(p instanceof ForkTsCheckerWebpackPlugin),
+    );
+
+    // Copy the options from the original ForkTsCheckerWebpackPlugin instance
+    const forkTsCheckerOptions = (existingForkTsChecker && existingForkTsChecker.options) || {};
+
+    // Increase memory limit
+    forkTsCheckerOptions.memoryLimit = 3072;
+
+    config.plugins.push(
+      new ForkTsCheckerWebpackPlugin(forkTsCheckerOptions),
       new MonacoEditorPlugin({
         languages: ['markdown'],
       }),
-    ],
+    );
   },
-
   pwa: {
     name: 'Advanced Markdown Editor',
   },
