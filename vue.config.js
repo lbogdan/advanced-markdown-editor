8ece212728e1643804b3239e398ca2f93c9bb1ba
Bogdan Luca <luca.bogdan@gmail.com>
2020-01-10 00:11:47 +0200

    Fix ts-checker.

--- vue.config.js
+++ vue.config.js
@@ -1,33 +1,16 @@
 /* eslint-disable no-param-reassign */
 
-const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
 const MonacoEditorPlugin = require('monaco-editor-webpack-plugin');
 
 module.exports = {
   configureWebpack: (config) => {
-    // Get a reference to the existing ForkTsCheckerWebpackPlugin
-    const existingForkTsChecker = config.plugins.filter(
-      (p) => p instanceof ForkTsCheckerWebpackPlugin,
-    )[0];
-
-    // Remove the existing ForkTsCheckerWebpackPlugin
-    config.plugins = config.plugins.filter(
-      (p) => !(p instanceof ForkTsCheckerWebpackPlugin),
-    );
-
-    // Copy the options from the original ForkTsCheckerWebpackPlugin instance
-    const forkTsCheckerOptions = (existingForkTsChecker && existingForkTsChecker.options) || {};
-
-    // Increase memory limit
-    forkTsCheckerOptions.memoryLimit = 3072;
-
     config.plugins.push(
-      new ForkTsCheckerWebpackPlugin(forkTsCheckerOptions),
       new MonacoEditorPlugin({
         languages: ['markdown'],
       }),
     );
   },
+
   pwa: {
     name: 'Advanced Markdown Editor',
   },
@@ -38,5 +21,13 @@ module.exports = {
 
   css: {
     sourceMap: true,
+  },
+
+  chainWebpack: (config) => {
+    config.plugin('fork-ts-checker').tap((options) => {
+      options[0].memoryLimit = 3072;
+      console.log(options);
+      return options;
+    });
   },
 };
